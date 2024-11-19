8e04e9ab717af95f2af6f9bef82b8b74e59a30e7
Diego Granados <diegranados@estudiantec.cr>
2024-11-19 17:42:11 -0600

    Comments 2
    X

--- Instruction.py
+++ Instruction.py
@@ -1,4 +1,4 @@
 class Instruction:
     def __init__(self, type, command):
-        self.type = type
-        self.command = command
+        self.type = type  # The type of instruction can be python, shell, or STOP to kill the Node.
+        self.command = command  # The command to execute in the Node.
